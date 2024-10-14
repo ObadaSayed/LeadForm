5c33223ffe07e8b4c32e60936442d1e80bf93bb4
Obada <Obada.Sayed92@Gmail.com>
2024-10-14 20:41:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('leadForm');
-const API_URL = 'https://thingproxy.freeboard.io/fetch/https://my349193.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection';
+const API_URL = 'https://my349193.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection';
 form.addEventListener('submit', async (event) => {
   event.preventDefault(); // Prevents form from submitting the default way
 
